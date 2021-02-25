f5f41d06abd3b310d443ae70cff42967334603c4
dkstm95 <dkstm95@naver.com>
2021-02-25 23:08:10 +0900

    Making MainController.

---
+++ src/main/java/Toy/Earningbell/Earningbell/controller/MainController.java
@@ -0,0 +1,13 @@
+package Toy.Earningbell.Earningbell.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String main() {
+        return "mainPage";
+    }
+}

a960e9ba0255de3c883026b563752da16ed8267e
dkstm95 <dkstm95@naver.com>
2021-02-25 22:32:25 +0900

    Initial Commit.

---
+++ src/test/java/Toy/Earningbell/Earningbell/EarningbellApplicationTests.java
@@ -0,0 +1,13 @@
+package Toy.Earningbell.Earningbell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarningbellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
